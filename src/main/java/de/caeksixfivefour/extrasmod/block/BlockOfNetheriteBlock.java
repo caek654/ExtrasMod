e1e8eece81d409dcc9aee579e73e06d244ef431e
caek654 <marce@DESKTOP-CHAGB6D>
2021-08-10 21:12:56 +0200

    Added - Block of Netherite

---
+++ src/main/java/de/caeksixfivefour/extrasmod/block/BlockOfNetheriteBlock.java
@@ -0,0 +1,49 @@
+
+package de.caeksixfivefour.extrasmod.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import java.util.List;
+import java.util.Collections;
+
+import de.caeksixfivefour.extrasmod.ExtrasModModElements;
+
+@ExtrasModModElements.ModElement.Tag
+public class BlockOfNetheriteBlock extends ExtrasModModElements.ModElement {
+	@ObjectHolder("extras_mod:block_of_netherite")
+	public static final Block block = null;
+	public BlockOfNetheriteBlock(ExtrasModModElements instance) {
+		super(instance, 3);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(20f, 10f).setLightLevel(s -> 0));
+			setRegistryName("block_of_netherite");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
